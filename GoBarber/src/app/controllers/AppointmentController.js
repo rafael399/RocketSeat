68287df2b5f022e0b7544d1ada823d83ac92d8f8
rafael399 <rafael399@gmail.com>
2019-10-24 14:37:23 -0300

    Add Nodemailer and e-mail templates

--- GoBarber/src/app/controllers/AppointmentController.js
+++ GoBarber/src/app/controllers/AppointmentController.js
@@ -5,6 +5,7 @@ import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notifications';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   async index(req, res) {
@@ -99,12 +100,12 @@ class AppointmentController {
 
     // Notify appointment provider
     const user = await User.findByPk(req.userId);
-    const formatedDate = format(hourStart, "'dia' dd 'de' MMMM 'as' H:mm'h'", {
+    const formattedDate = format(hourStart, "'dia' dd 'de' MMMM 'as' H:mm'h'", {
       locale: pt,
     });
 
     await Notification.create({
-      content: `Novo agendamento de ${user.name} para ${formatedDate}`,
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
     });
 
@@ -112,7 +113,20 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -131,6 +145,19 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM 'as' H:mm'h'", {
+          locale: pt,
+        }),
+      },
+    });
 
     return res.json(appointment);
   }
