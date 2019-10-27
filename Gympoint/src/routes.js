7fd137dc27ec3eeffcd097cccf23e5bcb7aa4565
rafael399 <rafael399@gmail.com>
2019-10-27 20:38:13 -0300

    added Plans and Registrations

--- Gympoint/src/routes.js
+++ Gympoint/src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import StudentController from './app/controllers/StudentController';
-// import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import PlanController from './app/controllers/PlanController';
+import RegistrationController from './app/controllers/RegistrationController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -9,7 +11,18 @@ const routes = new Router();
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
 routes.post('/students', StudentController.store);
 routes.put('/students/:studentId', StudentController.update);
+
+routes.get('/plan', PlanController.index);
+routes.post('/plan', PlanController.store);
+routes.put('/plan/:id', PlanController.update);
+routes.delete('/plan/:id', PlanController.delete);
+
+routes.get('/registration', RegistrationController.index);
+routes.post('/registration', RegistrationController.store);
+routes.put('/registration/:id', RegistrationController.update);
+routes.delete('/registration/:id', RegistrationController.delete);
 
 export default routes;
