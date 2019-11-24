e6b0f4cac5d966394c77a1d8c9307fa7c7c48dd3
rafael399 <rafael399@gmail.com>
2019-11-24 18:51:16 -0300

    Reactotron + Saga

--- Desafio7/src/config/ReactotronConfig.js
+++ Desafio7/src/config/ReactotronConfig.js
@@ -1,10 +1,12 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
+import reacotronSaga from 'reactotron-redux-saga';
 
 if (__DEV__) {
   const tron = Reactotron.configure()
     .useReactNative()
     .use(reactotronRedux())
+    .use(reacotronSaga())
     .connect();
 
   console.tron = tron;
