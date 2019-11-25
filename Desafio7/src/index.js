bbef62cc7fbacf44705aa1a894dc2f1a2168b853
rafael399 <rafael399@gmail.com>
2019-11-25 13:08:44 -0300

    Navigate to cart when adding a new product

--- Desafio7/src/index.js
+++ Desafio7/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-
 import { Provider } from 'react-redux';
-
 import './config/ReactotronConfig';
-import Routes from './routes';
+import { StatusBar } from 'react-native';
 import store from './store';
+
+import Routes from './routes';
+import NavigationService from './services/navigation';
 
 export default function App() {
   return (
     <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#141419" />
-      <Routes />
+      <Routes
+        ref={navigatorRef => NavigationService.setNavigator(navigatorRef)}
+      />
     </Provider>
   );
 }
 
-// ref={navigatorRef => NavigationService.setNavigatior(navigatorRed)}
+//
