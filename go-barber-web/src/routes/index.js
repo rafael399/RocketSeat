b81681bc64d35e7d5865ef28f6501fed4e6b1e51
rafael399 <rafael399@gmail.com>
2019-11-29 15:02:01 -0300

    Private routes

--- go-barber-web/src/routes/index.js
+++ go-barber-web/src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -10,10 +11,11 @@ import Profile from '../pages/Profile';
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact componente={SignIn} />
-      <Route path="/register" componente={SignUp} />
-      <Route path="/dashboard" componente={Dashboard} />
-      <Route path="/profile" componente={Profile} />
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }
