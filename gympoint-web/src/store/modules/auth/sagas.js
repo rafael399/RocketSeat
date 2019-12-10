9287046eae1735b57ba4d25a71192af20e6a0912
rafael399 <rafael399@gmail.com>
2019-12-10 13:16:06 -0300

    Authentication 'loading'

--- gympoint-web/src/store/modules/auth/sagas.js
+++ gympoint-web/src/store/modules/auth/sagas.js
@@ -3,21 +3,25 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signInFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, '/sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, '/sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  yield put(signInSuccess(token, user));
+    yield put(signInSuccess(token, user));
 
-  history.push('/students');
+    history.push('/students');
+  } catch (err) {
+    yield put(signInFailure());
+  }
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
