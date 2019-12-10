acf7b4dd3d4110637b25575874c528f8114b64d4
rafael399 <rafael399@gmail.com>
2019-12-10 13:30:59 -0300

    Authenticated requisitions

--- gympoint-web/src/pages/Students/index.js
+++ gympoint-web/src/pages/Students/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import api from '~/services/api';
 
 // import { Container } from './styles';
 
 export default function Students() {
+  api.get('/registration');
+
   return <h1>Students</h1>;
 }
--- gympoint-web/src/store/modules/auth/sagas.js
+++ gympoint-web/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -16,12 +17,29 @@ export function* signIn({ payload }) {
 
     const { token, user } = response.data;
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push('/students');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados.');
+
     yield put(signInFailure());
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('persist/REHYDRATE', setToken),
+]);
