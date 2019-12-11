986b7b846025f1d8b3ec53c8267c37de3a118010
rafael399 <rafael399@gmail.com>
2019-12-10 22:24:50 -0300

    Students page Style

--- gympoint-web/src/pages/Students/index.js
+++ gympoint-web/src/pages/Students/index.js
@@ -1,10 +1,76 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import { MdAddBox, MdSearch } from 'react-icons/md';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Content } from './styles';
 
 export default function Students() {
   api.get('/registration');
 
-  return <h1>Students</h1>;
+  return (
+    <Container>
+      <header>
+        <h1>Gerenciando alunos</h1>
+
+        <span>
+          <button type="button">
+            <MdAddBox size={22} color="#fff" />
+            <span>CADASTRAR</span>
+          </button>
+          <div>
+            <MdSearch size={18} color="#999" />
+            <input type="text" placeholder="Buscar aluno" />
+          </div>
+        </span>
+      </header>
+
+      <Content>
+        <table>
+          <tr>
+            <th>NOME</th>
+            <th>E-MAIL</th>
+            <th>IDADE</th>
+            <th />
+          </tr>
+          <tr>
+            <td>Aluno 1</td>
+            <td>email1@email.com</td>
+            <td>20</td>
+            <td>
+              <button type="button">editar</button>
+              <button type="button">apagar</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Aluno 1</td>
+            <td>email1@email.com</td>
+            <td>20</td>
+            <td>
+              <button type="button">editar</button>
+              <button type="button">apagar</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Aluno 1</td>
+            <td>email1@email.com</td>
+            <td>20</td>
+            <td>
+              <button type="button">editar</button>
+              <button type="button">apagar</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Aluno 1</td>
+            <td>email1@email.com</td>
+            <td>20</td>
+            <td>
+              <button type="button">editar</button>
+              <button type="button">apagar</button>
+            </td>
+          </tr>
+        </table>
+      </Content>
+    </Container>
+  );
 }
