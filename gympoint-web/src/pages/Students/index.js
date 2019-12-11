8ccb998d3675b74a4fb5b6eed8a3112589f6c466
rafael399 <rafael399@gmail.com>
2019-12-11 12:35:31 -0300

    Students page with api info

--- gympoint-web/src/pages/Students/index.js
+++ gympoint-web/src/pages/Students/index.js
@@ -1,12 +1,22 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdAddBox, MdSearch } from 'react-icons/md';
 import api from '~/services/api';
 
 import { Container, Content } from './styles';
 
 export default function Students() {
-  api.get('/registration');
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    async function loadStudents() {
+      const response = await api.get('/students');
+
+      setStudents(response.data);
+    }
+
+    loadStudents();
+  }, []);
 
   return (
     <Container>
@@ -33,42 +43,17 @@ export default function Students() {
             <th>IDADE</th>
             <th />
           </tr>
-          <tr>
-            <td>Aluno 1</td>
-            <td>email1@email.com</td>
-            <td>20</td>
-            <td>
-              <button type="button">editar</button>
-              <button type="button">apagar</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Aluno 1</td>
-            <td>email1@email.com</td>
-            <td>20</td>
-            <td>
-              <button type="button">editar</button>
-              <button type="button">apagar</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Aluno 1</td>
-            <td>email1@email.com</td>
-            <td>20</td>
-            <td>
-              <button type="button">editar</button>
-              <button type="button">apagar</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Aluno 1</td>
-            <td>email1@email.com</td>
-            <td>20</td>
-            <td>
-              <button type="button">editar</button>
-              <button type="button">apagar</button>
-            </td>
-          </tr>
+          {students.map(student => (
+            <tr>
+              <td>{student.name}</td>
+              <td>{student.email}</td>
+              <td>{student.age}</td>
+              <td>
+                <button type="button">editar</button>
+                <button type="button">apagar</button>
+              </td>
+            </tr>
+          ))}
         </table>
       </Content>
     </Container>
