347a869bc7c5bd2fadefe81a7fae866c4cc36eaf
rafael399 <rafael399@gmail.com>
2019-12-20 22:24:18 -0300

    Setting up redux

--- GympointMobile/src/index.js
+++ GympointMobile/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
+
+import './config/ReactotronConfig';
+
 import Routes from './routes';
 
-// import { Container } from './styles';
+import { store } from './store';
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-      <Routes />
+      <Provider store={store}>
+        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+        <Routes />
+      </Provider>
     </>
   );
 }
