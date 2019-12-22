84fe61d51a97e2552b558b8da8e7f4144c215a3d
rafael399 <rafael399@gmail.com>
2019-12-22 11:56:42 -0300

    Styling routes

--- GympointMobile/src/pages/CheckIn/index.js
+++ GympointMobile/src/pages/CheckIn/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import { Container } from './styles';
 
 export default function CheckIn() {
-  return <Text>CheckIn</Text>;
+  return <Container />;
 }
+
+CheckIn.navigationOptions = {
+  tabBarLabel: 'Check-ins',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="person-pin" size={22} color={tintColor} />
+  ),
+};
--- GympointMobile/src/routes.js
+++ GympointMobile/src/routes.js
@@ -5,15 +5,29 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 import SignIn from './pages/SignIn';
 
 import CheckIn from './pages/CheckIn';
+import HelpOrder from './pages/HelpOrder';
 
 export default (signedIn = false) =>
   createAppContainer(
     createSwitchNavigator(
       {
         Sign: createSwitchNavigator({ SignIn }),
-        App: createBottomTabNavigator({
-          CheckIn,
-        }),
+        App: createBottomTabNavigator(
+          {
+            CheckIn,
+            HelpOrder,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#EE4E62',
+              inactiveTintColor: '#999',
+              style: {
+                backgroundColor: '#fff',
+              },
+            },
+          }
+        ),
       },
       {
         initialRouteName: signedIn ? 'App' : 'Sign',
