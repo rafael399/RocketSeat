d47b9f7ef1e2e54207943ceb5a263515ff5a8a61
rafael399 <rafael399@gmail.com>
2019-12-26 13:47:26 -0300

    Orders page style

--- GympointMobile/src/pages/HelpOrders/Orders/index.js
+++ GympointMobile/src/pages/HelpOrders/Orders/index.js
@@ -1,30 +1,81 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+// import { TouchableOpacity } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
 import Header from '~/components/Header';
 
-import { Container } from './styles';
+import Button from '~/components/Button';
+
+import {
+  Container,
+  Content,
+  HelpOrderList,
+  HelpOrderItem,
+  HelpOrderHeader,
+  HelpOrderTitle,
+  OrderAnswered,
+  HelpOrderDate,
+  HelpOrderDescription,
+} from './styles';
 
 export default function Orders() {
+  const [orders, setOrders] = useState([
+    {
+      id: 1,
+      answered: true,
+      date: 'Hoje as 14h',
+      desc:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur veniam modi non pariatur, porro, unde similique, sequi a voluptas ea obcaecati quas delectus fuga repudiandae excepturi eveniet blanditiis explicabo dolorem?',
+    },
+    {
+      id: 2,
+      answered: false,
+      date: 'Hoje as 14h',
+      desc:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur veniam modi non pariatur, porro, unde similique, sequi a voluptas ea obcaecati quas delectus fuga repudiandae excepturi eveniet blanditiis explicabo dolorem?',
+    },
+  ]);
+
+  function handleNewHelpOrder() {}
+
   return (
     <Background>
       <Container>
         <Header />
+
+        <Content>
+          <Button onPress={handleNewHelpOrder}>Novo pedido de auxílio</Button>
+
+          <HelpOrderList
+            data={orders}
+            keyExtractor={item => String(item.id)}
+            renderItem={({ item }) => (
+              <HelpOrderItem>
+                <HelpOrderHeader>
+                  <HelpOrderTitle>
+                    <Icon
+                      name={
+                        item.answered ? 'check-box' : 'check-box-outline-blank'
+                      }
+                      size={22}
+                      color={item.answered ? '#42CB59' : '#999'}
+                    />
+                    <OrderAnswered answered={item.answered}>
+                      {item.answered ? 'Respondido' : 'Sem Resposta'}
+                    </OrderAnswered>
+                  </HelpOrderTitle>
+                  <HelpOrderDate>{item.date}</HelpOrderDate>
+                </HelpOrderHeader>
+                <HelpOrderDescription numberOfLines={3}>
+                  {item.desc}
+                </HelpOrderDescription>
+              </HelpOrderItem>
+            )}
+          />
+        </Content>
       </Container>
     </Background>
   );
 }
-
-// Orders.navigationOptions = ({ navigation }) => ({
-//   headerLeft: () => (
-//     <TouchableOpacity
-//       onPress={() => {
-//         navigation.navigate('Orders');
-//       }}>
-//       <Icon name="chevron-left" size={30} color="#EE4E62" />
-//     </TouchableOpacity>
-//   ),
-// });
--- GympointMobile/src/pages/HelpOrders/Orders/styles.js
+++ GympointMobile/src/pages/HelpOrders/Orders/styles.js
@@ -3,3 +3,60 @@ import styled from 'styled-components/native';
 export const Container = styled.SafeAreaView`
   flex: 1;
 `;
+
+export const Content = styled.View`
+  margin: 75px 20px 0;
+`;
+
+export const HelpOrderList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: 30,
+  },
+})`
+  margin-bottom: 30px;
+  margin-top: 20px;
+`;
+
+export const HelpOrderItem = styled.View`
+  flex-direction: column;
+
+  padding: 20px;
+  height: 150px;
+  margin-bottom: 10px;
+
+  background: #fff;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+`;
+
+export const HelpOrderHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-bottom: 10px;
+`;
+
+export const HelpOrderTitle = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const OrderAnswered = styled.Text`
+  color: ${props => (props.answered ? '#42CB59' : '#999')};
+  margin-left: 15px;
+
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+export const HelpOrderDate = styled.Text`
+  font-size: 14px;
+  color: #666;
+`;
+
+export const HelpOrderDescription = styled.Text`
+  color: #666;
+  line-height: 26px;
+`;
