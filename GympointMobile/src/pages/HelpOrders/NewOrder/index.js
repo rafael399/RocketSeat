67bd71dc4009e27f88dbff1f69e3c3885aed3160
rafael399 <rafael399@gmail.com>
2019-12-27 13:12:22 -0300

    NewOrder page

--- GympointMobile/src/pages/HelpOrders/NewOrder/index.js
+++ GympointMobile/src/pages/HelpOrders/NewOrder/index.js
@@ -1,18 +1,63 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { TouchableOpacity, Alert } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '~/services/api';
 
 import Background from '~/components/Background';
 import Header from '~/components/Header';
 
-import { Container } from './styles';
+import { Container, Content, FormInput, SubmitButton } from './styles';
 
-export default function NewOrder() {
+export default function NewOrder({ navigation }) {
+  const [question, setQuestion] = useState('');
+  const [loading, setLoading] = useState(false);
+  const studentId = useSelector(state => state.student.student.id);
+
+  async function handleSubmit() {
+    try {
+      setLoading(true);
+
+      await api.post(`/students/${studentId}/help-orders`, {
+        question,
+      });
+
+      setLoading(false);
+
+      Alert.alert('Sucesso', 'Seu pedido de ajuda foi enviado com sucesso.');
+
+      navigation.goBack();
+    } catch (err) {
+      setLoading(false);
+
+      Alert.alert(
+        'Falha na autenticação',
+        'Houve um erro no login, verifique seus dados'
+      );
+    }
+  }
+
   return (
     <Background>
       <Container>
         <Header />
+
+        <Content>
+          <FormInput
+            placeholder="Inclua seu pedido de auxílio"
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={question}
+            onChangeText={setQuestion}
+            multiline
+          />
+
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Enviar pedido
+          </SubmitButton>
+        </Content>
       </Container>
     </Background>
   );
--- GympointMobile/src/pages/HelpOrders/NewOrder/styles.js
+++ GympointMobile/src/pages/HelpOrders/NewOrder/styles.js
@@ -1,5 +1,37 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView`
+import Button from '~/components/Button';
+
+export const Container = styled.KeyboardAvoidingView.attrs({
+  enabled: Platform.OS === 'ios',
+  behavior: 'padding',
+})`
   flex: 1;
+`;
+
+export const Content = styled.View`
+  margin: 75px 20px 0;
+`;
+
+// export const Form = styled.View`
+//   /* align-self: stretch; */
+// `;
+
+export const FormInput = styled.TextInput`
+  height: 300px;
+  background: #fff;
+
+  font-size: 16px;
+  line-height: 21px;
+  color: #999;
+
+  border: 1px solid #ddd;
+  border-radius: 4px;
+
+  padding-left: 20px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 20px;
 `;
